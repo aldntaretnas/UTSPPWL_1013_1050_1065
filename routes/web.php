ee26cc11f8d994ff0e5e604dd6eb96034362751e
aldntaretnas <aldinataretnas24@gmail.com>
2025-03-27 08:29:48 +0700

    ini file routesnya

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [ProductController::class, 'index'])->name('index');
+Route::get('/products', [ProductController::class, 'index'])->name('index');
+Route::get('products/create', [ProductController::class,'create'])->name('products.create');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+Route::get('products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('products/{id}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('products/{id}', [ProductController::class,'destroy'])->name('products.destroy');
+
+Route::get('/brand', [BrandController::class, 'index'])->name('brand');
+Route::get('/categories', [CategoriesController::class, 'index'])->name('categories');
