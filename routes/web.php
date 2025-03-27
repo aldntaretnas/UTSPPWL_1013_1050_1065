2fd4508575db4330bcda927ebc947503b6e3b0ed
aldntaretnas <aldinataretnas24@gmail.com>
2025-03-27 08:40:29 +0700

    Update web.php
    ini file routesnya

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,19 @@
 <?php
 
-use App\Http\Controllers\BrandController;
-use App\Http\Controllers\CategoriesController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\MerekController;
+use App\Http\Controllers\kategoriController;
+use App\Http\Controllers\ProdukController;
 use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', [ProductController::class, 'index'])->name('index');
-Route::get('/products', [ProductController::class, 'index'])->name('index');
-Route::get('products/create', [ProductController::class,'create'])->name('products.create');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-Route::get('products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::put('products/{id}', [ProductController::class, 'update'])->name('products.update');
-Route::delete('products/{id}', [ProductController::class,'destroy'])->name('products.destroy');
+Route::get('/', [ProdukController::class, 'index'])->name('index');
+Route::get('/products', [ProdukController::class, 'index'])->name('index');
+Route::get('products/create', [ProdukController::class,'create'])->name('products.create');
+Route::post('/products', [ProdukController::class, 'store'])->name('products.store');
+Route::get('products/{id}/edit', [ProdukController::class, 'edit'])->name('products.edit');
+Route::put('products/{id}', [ProdukController::class, 'update'])->name('products.update');
+Route::delete('products/{id}', [ProdukController::class,'destroy'])->name('products.destroy');
 
-Route::get('/brand', [BrandController::class, 'index'])->name('brand');
-Route::get('/categories', [CategoriesController::class, 'index'])->name('categories');
+Route::get('/brand', [MerekController::class, 'index'])->name('brand');
+Route::get('/categories', [KategoriController::class, 'index'])->name('categories');
